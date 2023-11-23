430957f5ea6d9ec12e3646d97ebb8e934fb959c6
Ajiex <ari.wbo.aji@gmail.com>
2023-11-23 08:27:55 +0700

    --edit route

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
